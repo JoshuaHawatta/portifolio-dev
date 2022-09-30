aea7cc29169b77b025dfc7bc04c61a830469b7a9
Joshua Hawatta Azevedo <joshuahawattaazevedo@MacBook-Pro-de-Joshua.local>
2022-09-30 00:57:33 -0300

    created initial routes components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import ROUTES_PATHS from './Routes';
+import Header from './components/Header';
+
 const App = () => (
-    <h1>Hello World!</h1>
+    <Router>
+        <Header />
+        <ROUTES_PATHS />
+    </Router>
 )
 
 export default App
---
+++ src/Routes.jsx
@@ -0,0 +1,10 @@
+import { Routes, Route } from 'react-router-dom';
+
+const ROUTES_PATHS = () => (
+    <Routes>
+        <Route path='/' element={ <h1>Hello, World!</h1> } />
+    </Routes>
+  
+)
+
+export default ROUTES_PATHS
