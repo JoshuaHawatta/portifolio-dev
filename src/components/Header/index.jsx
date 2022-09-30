527f25609686727d20b51e8a24eeffafbac403e7
Joshua Hawatta Azevedo <joshuahawattaazevedo@MacBook-Pro-de-Joshua.local>
2022-09-30 00:58:06 -0300

    created header component file

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,15 @@
+const Header = () => {
+    return(
+        <header>
+            <div>
+
+            </div>
+
+            <nav>
+                <h1>sadasda</h1>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
