9af97b3781c079a25c73a19689dddb3392d98f20
Joshua Hawatta Azevedo <joshuahawattaazevedo@MacBook-Pro-de-Joshua.local>
2022-09-30 23:08:25 -0300

    renamed css initial file

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+import './setup.css';
 
 ReactDOM.createRoot(document.querySelector('#root')).render(
     <React.StrictMode>
