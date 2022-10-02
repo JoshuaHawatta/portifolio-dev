74d3c48e9b28e1ba2f3d3b0c56748f2684ae1782
Joshua Hawatta Azevedo <joshuahawattaazevedo@MacBook-Pro-de-Joshua.local>
2022-10-02 00:41:23 -0300

    imported footer to App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 import ROUTES_PATHS from './Routes';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => (
     <Router>
         <Header />
         <ROUTES_PATHS />
+        <Footer />
     </Router>
 )
 
