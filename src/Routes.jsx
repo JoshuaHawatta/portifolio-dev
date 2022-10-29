9d94089e017e1cafc60b5bb08cf635171ef4037c
Joshua Hawatta Azevedo <joshuahawattaazevedo@MacBook-Pro-de-Joshua.local>
2022-10-29 03:49:37 -0300

    created the Work and Resumé Pages

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,9 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/Home';
+import WorksPage from './pages/Works';
+import ResumePage from './pages/Resume';
 
 const ROUTES_PATHS = () => (
     <Routes>
         <Route path='/' element={ <HomePage /> } />
+        <Route path='/works' element={ <WorksPage /> } />
+        <Route path='/resume' element={ <ResumePage /> } />
     </Routes>
   
 )
