e75e0e9e1e3e544876c31b1bb7d7dfcdf8dd6215
Joshua Hawatta Azevedo <joshuahawattaazevedo@MacBook-Pro-de-Joshua.local>
2022-10-31 10:32:49 -0300

    minor change at the Icon With Text Component

--- src/components/IconWithText/index.jsx
+++ src/components/IconWithText/index.jsx
@@ -3,7 +3,7 @@ import { StackWrapper } from './Styles';
 const ICON_WITH_TEXT_JSX = ({ stackIcon, title, percentage }) => (
     <StackWrapper>
         { stackIcon }
-        <h5>{ title } - { percentage }%</h5>
+        <h5>{ title } { percentage }%</h5>
     </StackWrapper>
 )
 
