2b195d85272ce0e219b89bcb04a4f0c5032e3380
Joshua Hawatta Azevedo <joshuahawattaazevedo@MacBook-Pro-de-Joshua.local>
2022-10-31 10:32:24 -0300

    removed the footer from the site

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import ROUTES_PATHS from './Routes';
 import Header from './components/Header';
-import Footer from './components/Footer';
 
 const App = () => (
     <Router>
         <Header />
         <ROUTES_PATHS />
-        <Footer />
     </Router>
 )
 
