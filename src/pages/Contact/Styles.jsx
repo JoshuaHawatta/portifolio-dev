030d6c2d450d981185e3e8763850221bf9504858
Joshua Hawatta Azevedo <joshuahawattaazevedo@MacBook-Pro-de-Joshua.local>
2022-11-05 17:31:40 -0300

    Finished Contact responsivness

--- src/pages/Contact/Styles.jsx
+++ src/pages/Contact/Styles.jsx
@@ -7,30 +7,45 @@ export const ArticleWrapper = styled.article `
     width: 100%;
     height: 100vh;
     padding: 0 12rem;
-    background: var(--gray-color);
+    background: var(--blue-color);
+
+    aside {
+        display: none;
+
+        /*CHILDREN_ELEMENTS*/
+        button {
+            width: 60%;
+            position: absolute;
+            top: 5rem;
+            left: 8rem;
+        }
+    }
+
+    /*MOBILE*/
+    @media (max-width: 779px) {
+        flex-flow: column;
+        justify-content: center;
+        padding: 0 6rem;
+
+        /*CHILDREN_ELEMENTS*/
+        aside { display: unset; }
+        height: 140vh;
+    }
 
     /*MOBILE_LAND_SCAPE_WITH_BUTTONS*/
     @media (max-height: 520px) and (orientation: landscape) { display: none; }
 `;
 
-export const Line = styled.div `
-    height: 75vh;
-    width: .1rem;
-    margin: 0 auto;
-    background-color: var(--blue-color);
-`;
-
-export const Concats = styled.section `
+export const Conctats = styled.section`
     display: flex;
     flex-flow: column nowrap;
     align-items: center;
     justify-content: space-between;
     width: 30vw;
-    height: 75vh;
+    height: 78vh;
     background: #FFFFFF;
     padding: 4rem;
-    border-radius: .4rem;
-
+    border-radius: 3rem;
 
     /*CHILDREN_ELEMENTS*/
     h4 {
@@ -48,16 +63,188 @@ export const Concats = styled.section `
         height: 100vh;
 
         /*CHILDREN_ELEMENTS*/
-        a{
+        a {
             width: 100%;
             text-decoration: none;
             
             /*CHILDREN_ELEMENTS*/
             button { margin: 1rem 0; }
         }
-
-       
     }
 
-    h1 { color: red; }
+    /*MOBILE*/
+    @media (max-width: 779px) {
+        display: flex;
+        width: 100%;
+        overflow: hidden;
+        height: 50vh;
+        margin-bottom: 6rem;
+
+        /*CHILDREN_ELEMENTS*/
+        h4 { font-size: 2.2rem; }
+    }
+
+    /*TABLETS*/
+    @media (max-width: 779px) and (min-height: 900px) {
+        /*CHILDREN_ELEMENTS*/
+        h4 { font-size: 3.5rem; }
+
+        div {
+            display: flex;
+            width: 100%;
+            justify-content: center;
+            transform: translateY(-11rem);
+
+            button { width: 100%; }
+        }
+    }
+`;
+
+export const Line = styled.div `
+    height: 75vh;
+    width: .1rem;
+    margin: 0 auto;
+    background-color: var(--green-color);
+
+    /*MOBILE*/
+    @media (max-width: 779px) { display: none; }
+`;
+
+export const FormWrapper = styled.section `
+    display: flex;
+    flex-flow: column wrap;
+    justify-content: center;
+    align-items: center;
+    background: #FFFFFF;
+    border-radius: 3rem;
+    width: 30vw;
+    padding: 4rem 0;
+
+    /*CHILDREN_ELEMENTS*/
+    h4 {
+        font: 2.8rem now-light, Helvetica, sans-serif;
+        color: var(--blue-color);
+    }
+
+    /*MOBILE*/
+    @media (max-width: 779px) {
+        width: 100%;
+
+        /*CHILDREN_ELEMENTS*/
+        h4 { font-size: 2.2rem; }
+    }
+
+    /*TABLETS*/
+    @media (max-width: 779px) and (min-height: 900px) {
+        /*CHILDREN_ELEMENTS*/
+        h4 { font-size: 3.5rem; }
+    }
+`;
+
+export const FormSection = styled.section `
+    display: flex;
+    justify-content: center;
+    width: 100%;
+
+    /*CHILDREN_ELEMENTS*/
+    div {
+        display: flex;
+        flex-flow: column wrap;
+        margin-bottom: .5rem;
+
+        /*CHILDREN_ELEMENTS*/
+        label {
+            font: 1.6rem now-light, Helvetica, sans-serif;
+            color: var(--blue-color);
+            text-align: center;
+            margin: .8rem 0;
+            padding: .5rem 0;
+            transform: translateY(1.8rem);
+            background: #FFFFFF;
+            width: 35%;
+        }
+
+        input, textarea {
+            outline: none;
+            font: 1.4rem now-light, Helvetica, sans-serif;
+            width: 30rem;
+            border-radius: .4rem;
+            padding: 2rem 1rem;
+            border: .1rem solid var(--blue-color);
+        }
+
+        input {
+            color: #000000;
+            height: 5vh;
+            transition: .3s ease-out;
+
+            /*SIBLINGS*/
+            & + div {
+                width: 100%;
+                height: .1rem;
+                background-color: #FFFFFF;
+            }
+        }
+
+        textArea {
+            resize: none;
+            height: 19vh;
+        }
+    }
+
+    /*MOBILE*/
+    @media (max-width: 779px) {
+       width: 100%;
+
+       /*CHILDREN_ELEMENTS*/
+        div {
+            padding: 0 2rem;
+            margin: 0 2rem;
+            width: 100%;
+            justify-content: center;
+
+            /*CHILDREN_ELEMENTS*/
+            input, textarea { width: 100%; }
+
+            textarea { height: 12vh; }
+
+            label { font-size: 1.2rem; }
+        }
+    }
+
+    /*TABLETS*/
+    @media (max-width: 779px) and (min-height: 900px) {
+        /*CHILDREN_ELEMENTS*/
+        div > label { font-size: 1.5rem; }
+    }
+`;
+
+export const SendEmailButton = styled.button.attrs({ type: 'submit' }) `
+    font: 2rem now-light, Helvetica, sans-serif;
+    color: var(--blue-color);
+    background: none;
+    outline: none;
+    border: none;
+    text-decoration: underline;
+    margin-top: 2rem;
+    width: 11vw;
+    cursor: pointer;
+`;
+
+export const FooterLinksSection = styled.section `
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: flex-end;
+    align-items: center;
+    position: absolute;
+    padding: 1rem 4rem;
+    width: 100%;
+    bottom: 0;
+
+    /*CHILDREN_ELEMENTS*/
+    a {
+        font-size: 2rem;
+        margin: 0 .8rem;
+        color: var(--green-color);
+    }
 `;
