58d184d5ebe3e9be6acb1f97ac690ac9a438f774
Joshua Hawatta <joshuahawattaazevedo@gmail.com>
2023-03-04 20:46:53 -0300

    chore: minor changes at home file

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,39 +1,39 @@
-import { MainWrapper, Presentation, WhoIAm, PresentationImage } from './Styles';
-import PresentationImageUrl from '../../assets/images/presentation-image.jpg';
-import { useNavigate } from 'react-router-dom';
-import { FaPaperPlane } from 'react-icons/fa';
-import CTA_BUTTON_JSX from '../../components/CtaButton';
+import { MainWrapper, Presentation, WhoIAm, PresentationImage } from './Styles'
+import PresentationImageUrl from '../../assets/images/presentation-image.jpg'
+import { useNavigate } from 'react-router-dom'
+import { FaPaperPlane } from 'react-icons/fa'
+import CTA_BUTTON_JSX from '../../components/CtaButton'
 
 const HomePage = () => {
-    const navigate = useNavigate();
+	const navigate = useNavigate()
 
-    return (
-        <MainWrapper>
-            <Presentation>
-                <WhoIAm>
-                    <div>
-                        <h1>D<span>e</span>senvolvedor Full-Stack</h1>
-                        <p>Eu crio interfaces para sites, web-apps e afins.</p>
-                        <p>Atualmente estudo Node.js e SQL para a criação de APIs.</p>
-                        
-                        <p>
-                            Gosto de usar esse superpoder para ajudar você a criar seu site!
-                        </p>
+	return (
+		<MainWrapper>
+			<Presentation>
+				<WhoIAm>
+					<div>
+						<h1>
+							D<span>e</span>senvolvedor Full-Stack
+						</h1>
+						<p>Eu crio interfaces para sites, web-apps e afins.</p>
+						<p>Atualmente estudo Node.js e SQL para a criação de APIs.</p>
 
-                        <CTA_BUTTON_JSX
-                            icon={ <FaPaperPlane /> }
-                            text='Me chame!'
-                            action={ () => navigate('/contact') }
-                        />
-                    </div>
-                </WhoIAm>
+						<p>Gosto de usar esse superpoder para ajudar você a criar seu site!</p>
 
-                <PresentationImage>
-                    <img src={ PresentationImageUrl } alt='presentation-image' />
-                </PresentationImage>
-            </Presentation>
-        </MainWrapper>
-    )
+						<CTA_BUTTON_JSX
+							icon={<FaPaperPlane />}
+							text='Me chame!'
+							action={() => navigate('/contact')}
+						/>
+					</div>
+				</WhoIAm>
+
+				<PresentationImage>
+					<img src={PresentationImageUrl} alt='presentation-image' />
+				</PresentationImage>
+			</Presentation>
+		</MainWrapper>
+	)
 }
 
 export default HomePage
