70b9b0a1e472a7c2157716ea13e377ececb69133
Joshua Hawatta <joshuahawattaazevedo@gmail.com>
2023-03-05 23:22:55 -0300

    chore: file touched by prettier

--- src/components/CtaButton/Styles.jsx
+++ src/components/CtaButton/Styles.jsx
@@ -1,65 +1,64 @@
-import styled from "styled-components"
+import styled from 'styled-components'
 
-export const CallToActionButton = styled.button `
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    font: 2.3rem now-light, Helvetica, sans-serif;
-    z-index: 1;
+export const CallToActionButton = styled.button`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	height: 5rem;
+	border-radius: 0.4rem;
+	font: 2.3rem now-light, Helvetica, sans-serif;
+	z-index: 1;
 
-    background: ${({ outlined, color }) => {
-        if(outlined) {
-            return 'linear-gradient(to left, rgba(0, 0, 0, 0) 50%, #FFFFFF 50%) right'
-        }
-        else if (color) {
-            return `linear-gradient(to left, rgba(0, 0, 0, 0) 50%, #${ color } 50%) right`
-        }
+	background: ${({ outlined, color }) => {
+		if (outlined) {
+			return 'linear-gradient(to left, rgba(0, 0, 0, 0) 50%, #FFFFFF 50%) right'
+		} else if (color) {
+			return `linear-gradient(to left, rgba(0, 0, 0, 0) 50%, #${color} 50%) right`
+		}
 
-        return 'linear-gradient(to left, var(--blue-color) 50%, #FFFFFF 50%) right'
-    }};
+		return 'linear-gradient(to left, var(--blue-color) 50%, #FFFFFF 50%) right'
+	}};
 
-    border: ${ ({ color }) => color ? `.1rem solid #${ color }` : `.1rem solid #FFFFFF` };
-    outline: none;
-    width: ${ ({btnWidth}) => !btnWidth ? '22rem' : btnWidth }; 
-    height: 3rem;
-    padding: 2.5rem;
-    margin-top: 5rem;
-    padding-top: 2.8rem;
-    border-radius: .4rem;
-    background-size: 200% 100%;
-    transition: .3s ease-out;
-    color: ${ ({ color }) => color ? `#${ color }` : `#FFFFFF` };;
+	border: ${({ color }) => (color ? `.1rem solid #${color}` : `.1rem solid #FFFFFF`)};
+	outline: none;
+	width: ${({ btnWidth }) => (!btnWidth ? '22rem' : btnWidth)};
 
-    /*CHILDREN_ELEMENTS*/
-    svg { margin-right: 1rem; }
+	background-size: 200% 100%;
+	transition: 0.3s ease-out;
+	color: ${({ color }) => (color ? `#${color}` : `#FFFFFF`)};
 
-    /*EFFECTS*/
-    &:hover {
-        background-position: left;
-        color: var(--blue-color);
-        cursor: pointer;
+	/*CHILDREN_ELEMENTS*/
+	svg {
+		margin-right: 1rem;
+	}
 
-        color: ${({ outlined, color }) => {
-            if (outlined) return '#000000'
-            else if (color) return '#FFFFFF'
+	/*EFFECTS*/
+	&:hover {
+		background-position: left;
+		color: var(--blue-color);
+		cursor: pointer;
 
-            return 'var(--blue-color)'
-        }};
-    }
+		color: ${({ outlined, color }) => {
+			if (outlined) return '#000000'
+			else if (color) return '#FFFFFF'
 
-    /*MOBILE*/
-    @media (max-width: 779px) {
-        margin-top: 2rem;
-        width: 18rem;
-        padding: 2rem;
-        font-size: 2rem;
-    }
+			return 'var(--blue-color)'
+		}};
+	}
 
-    /*TABLETS*/
-    @media (min-width: 780px) and (max-width: 1015px) {
-        margin-top: 2rem;
-        width: 18rem;
-        padding: 2rem;
-        font-size: 2rem;
-    }
-`;
+	/*MOBILE*/
+	@media (max-width: 779px) {
+		margin-top: 2rem;
+		width: 18rem;
+		padding: 2rem;
+		font-size: 2rem;
+	}
+
+	/*TABLETS*/
+	@media (min-width: 780px) and (max-width: 1015px) {
+		margin-top: 2rem;
+		width: 18rem;
+		padding: 2rem;
+		font-size: 2rem;
+	}
+`
