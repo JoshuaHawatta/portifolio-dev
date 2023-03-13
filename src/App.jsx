e39351ca66f0b97abbef25711e1ada8dfbbe92e3
Joshua Hawatta <joshuahawattaazevedo@gmail.com>
2023-03-13 16:59:44 -0300

    chore: removing unnecessary code from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import { BrowserRouter as Router } from 'react-router-dom'
 import ROUTES_PATHS from './Routes'
-import Header from './components/Header'
+import HEADER_JSX from './components/Header'
 
 const App = () => (
 	<Router>
-		<h6>
-			P<span>o</span>r favor, me deixe em vertical!
-		</h6>
-		<Header />
+		<HEADER_JSX />
 		<ROUTES_PATHS />
 	</Router>
 )
