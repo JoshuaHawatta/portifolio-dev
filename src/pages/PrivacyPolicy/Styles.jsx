2dd68fc800dbaaa423949545674fc8e7356335bc
Joshua Hawatta <joshuahawattaazevedo@gmail.com>
2023-03-18 16:08:53 -0300

    chore: finishing privacy policy page screens

--- src/pages/PrivacyPolicy/Styles.jsx
+++ src/pages/PrivacyPolicy/Styles.jsx
@@ -34,23 +34,105 @@ export const StyledPrivacyPolicyArticle = styled.article`
 	}
 
 	/*MOBILE*/
-	@media (max-width: 779px) {
-		padding: 12rem 4rem;
+	@media (max-width: 500px) {
+		padding: 8rem 2rem;
 
 		/*CHILDREN_ELEMENTS*/
-		h1 {
-			font-size: 6rem;
-		}
+		section {
+			text-align: start;
 
-		h2 {
-			font-size: 3rem;
-		}
+			h1 {
+				width: 100%;
+				font-size: 11vw;
+			}
 
-		p {
-			font-size: 1.3rem;
-			width: 100%;
+			h2 {
+				font-size: 8vw;
+			}
+
+			p {
+				transform: translateY(5vh);
+				font-size: 4.2vw;
+				width: 100%;
+			}
+		}
+	}
+
+	@media (max-width: 500px) and (orientation: landscape) {
+		padding: 7rem 4rem;
+		height: 150vh;
+
+		/*CHILDREN_ELEMENTS*/
+		section {
+			text-align: start;
+
+			h1 {
+				width: 100%;
+				font-size: 9vw;
+				margin-bottom: 2rem;
+			}
+
+			h2 {
+				font-size: 5.4vw;
+			}
+
+			p {
+				transform: translateY(5vh);
+				font-size: 3vw;
+				width: 100%;
+			}
+		}
+	}
+
+	//TABLET
+	@media (min-width: 501px) and (max-width: 1100px) {
+		padding: 8rem;
+		height: 130vh;
+
+		/*CHILDREN_ELEMENTS*/
+		section {
+			text-align: start;
+
+			h1 {
+				width: 100%;
+				font-size: 9vw;
+			}
+
+			h2 {
+				font-size: 5vw;
+			}
+
+			p {
+				transform: translateY(5vh);
+				font-size: 3.6vw;
+				width: 100%;
+			}
+		}
+	}
+
+	@media (min-width: 501px) and (max-width: 1100px) and (orientation: landscape) {
+		padding: 7rem 4rem;
+		height: 150vh;
+
+		/*CHILDREN_ELEMENTS*/
+		section {
+			text-align: start;
+
+			h1 {
+				width: 100%;
+				font-size: 8vw;
+				margin-bottom: 2rem;
+			}
+
+			h2 {
+				font-size: 4.6vw;
+			}
+
+			p {
+				transform: translateY(5vh);
+				font-size: 2.3vw;
+				width: 100%;
+			}
 		}
 	}
 `
-
-export const StyledPrivacyPolicySection = styled.section``
--- src/pages/PrivacyPolicy/index.jsx
+++ src/pages/PrivacyPolicy/index.jsx
@@ -1,14 +1,14 @@
-import { StyledPrivacyPolicyArticle, StyledPrivacyPolicySection } from './Styles'
+import { StyledPrivacyPolicyArticle } from './Styles'
 
 const PrivacyPolicyPage = () => {
 	return (
 		<StyledPrivacyPolicyArticle>
-			<StyledPrivacyPolicySection>
+			<section>
 				<h1>
 					P<span>o</span>lítica de privacidade
 				</h1>
 
-				<StyledPrivacyPolicySection>
+				<section>
 					<h2>
 						Ao utilizar nossos serviços, você entende que <span>não</span> coletaremos ou
 						usaremos suas informações pessoais.
@@ -20,8 +20,8 @@ const PrivacyPolicyPage = () => {
 						consumeristas da <span>Lei Federal 8078/1990</span> e as demais normas do
 						ordenamento jurídico brasileiro aplicáveis.
 					</p>
-				</StyledPrivacyPolicySection>
-			</StyledPrivacyPolicySection>
+				</section>
+			</section>
 		</StyledPrivacyPolicyArticle>
 	)
 }
